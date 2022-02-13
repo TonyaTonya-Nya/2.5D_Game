8fb948aeaf6bfb88a3748878c2647bdd716ff238
Fengleaf <as61002@gmail.com>
2022-02-13 17:21:04 +0800

    第二關接第三關

---
+++ Assets/Scripts/Object/Item.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public float recoverAmount;
+    public AudioSource audioSource;
+    public AudioClip audioClip;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            AudioSource.PlayClipAtPoint(audioClip, Camera.main.transform.position, 1);
+            FindObjectOfType<PlayerManager>().Damage(-recoverAmount);
+            Destroy(gameObject);
+        }
+    }
+}
